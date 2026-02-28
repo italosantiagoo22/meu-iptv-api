5b0ae721a220cdfaf8d2d82c78ec654995121553
italosantiagoo22 <italosantiagoo22@gmail.com>
2026-02-28 07:05:35 +0000

    ajuste scripts

--- api/index.py
+++ api/index.py
@@ -14,21 +14,35 @@ def handler(request):
     headers = {"User-Agent": "Mozilla/5.0"}
 
     try:
+        # 1. Headers de disfarce (Dizemos que somos o próprio Dailymotion)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+            "Referer": "https://www.dailymotion.com/",
+            "Origin": "https://www.dailymotion.com"
+        }
+
         response = requests.get(url_embed, headers=headers).text
+        # Busca o link .m3u8 no código da página
         match = re.search(r'"url":"(https://.*?\.m3u8.*?)"', response)
 
         if match:
             stream_url = match.group(1).replace("\\/", "/")
+            
             return {
                 "statusCode": 302,
                 "headers": {
-                    "Location": stream_url
+                    "Location": stream_url,
+                    # LIBERA O ACESSO PARA O SMARTERS WEB
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Cache-Control": "no-cache, no-store, must-revalidate"
                 }
             }
         else:
             return {
                 "statusCode": 404,
-                "body": "Stream não encontrado"
+                "body": "Stream não encontrado no código da página"
             }
 
     except Exception as e:
